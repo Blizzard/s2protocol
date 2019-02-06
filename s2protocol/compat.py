e5fe28fe068be0cf427ed0d48c1f0a3b728a917f
Kang Hyojun <iam.kanghyojun@gmail.com>
2019-02-06 15:44:07 +0900

    The binary type includes int

--- s2protocol/compat.py
+++ s2protocol/compat.py
@@ -4,7 +4,7 @@ __all__ = 'byte_to_int',
 
 
 def byte_to_int(x):
-    if sys.version_info.major == 3 and isinstance(x, bytes):
+    if sys.version_info.major == 3 and isinstance(x, (bytes, int)):
         return x
     else:
         return ord(x)
